49606e23fbe35d6405d58de76f25b7dfe2ad15e7
Bongi90 <avalashiya90@gmail.com>
2025-09-02 09:53:15 +0200

    Your message here

--- backend_capstone/urls.py
+++ backend_capstone/urls.py
@@ -1,13 +1,18 @@
+
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
+
+    path("api/", include("tasks.urls")),  
+
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/users/", include("tasks.user_urls")),
-    path("api/tasks/", include("tasks.urls")),
-    path("api/kids/", include("kids_todo.urls")),
-    path("api/library/", include("library.urls")),
 ]
+
